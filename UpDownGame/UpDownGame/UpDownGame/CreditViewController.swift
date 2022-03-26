322183fc8eee28c394eafc56bdd82608878c00d9
Ji won Choi <79574342+ziuge@users.noreply.github.com>
2022-03-26 23:37:02 +0900

    Add files via upload

---
+++ UpDownGame/UpDownGame/UpDownGame/CreditViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CreditViewController.swift
+//  UpDownGame
+//
+//  Created by Choi on 2021/10/14.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+    
+    @IBAction func touchUpDismissButton(_ sender: UIButton){
+        dismiss(animated: true, completion: nil)
+    }
+}
