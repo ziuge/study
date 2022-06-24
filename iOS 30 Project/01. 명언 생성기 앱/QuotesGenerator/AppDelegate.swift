eacedea22dfb03fb1a742dd1aed3f04137456b66
Ji won Choi <79574342+ziuge@users.noreply.github.com>
2022-06-24 16:01:06 +0900

    Add files via upload

---
+++ iOS 30 Project/01. 명언 생성기 앱/QuotesGenerator/AppDelegate.swift
@@ -0,0 +1,37 @@
+//
+//  AppDelegate.swift
+//  QuotesGenerator
+//
+//  Created by Choi on 2022/02/05.
+//
+
+import UIKit
+
+@main
+//#28    0x0000000106e81707 in static AppDelegate.$main() at /Users/choi/Documents/개발공부/30개프로젝트/QuotesGenerator/QuotesGenerator/AppDelegate.swift:10
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+        // Called when a new scene session is being created.
+        // Use this method to select a configuration to create the new scene with.
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+        // Called when the user discards a scene session.
+        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+
+
+}
+
