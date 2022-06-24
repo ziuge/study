09905be651ec8f2cf79e001926231374a467444a
Ji won Choi <79574342+ziuge@users.noreply.github.com>
2022-06-24 16:03:47 +0900

    Add files via upload

---
+++ iOS 30 Project/05. 일기 앱/Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by Choi on 2022/02/14.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
---
+++ iOS 30 Project/05. 일기 앱/Diary/DiaryDetailViewController.swift
@@ -0,0 +1,93 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by Choi on 2022/02/14.
+//
+
+// 25분까지 봄
+import UIKit
+
+protocol DiaryDetailViewDelegate: AnyObject {
+    func didSelectDelete(indexPath: IndexPath)
+    func didSelectStar(indexPath: IndexPath, isStar: Bool)
+}
+
+class DiaryDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    weak var delegate: DiaryDetailViewDelegate?
+    var starButton: UIBarButtonItem?
+    
+    var diary: Diary?
+    var indexPath: IndexPath?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureView()
+    }
+    
+    private func configureView() {
+        guard let diary = self.diary else { return }
+        self.titleLabel.text = diary.title
+        self.contentsTextView.text = diary.contents
+        self.dateLabel.text = self.dateToString(date: diary.date)
+        self.starButton = UIBarButtonItem(image: nil, style: .plain, target: self, action: #selector(tapStarButton))
+        self.starButton?.image = diary.isStar ? UIImage(systemName: "star.fill") : UIImage(systemName: "star")
+        self.starButton?.tintColor = .orange
+        self.navigationItem.rightBarButtonItem = self.starButton
+    }
+    
+    private func dateToString(date: Date) -> String {
+        let formmater = DateFormatter()
+        formmater.dateFormat = "yy년 MM월 dd일 (EEEEE)"
+        formmater.locale = Locale(identifier: "ko_KR")
+        return formmater.string(from: date)
+    }
+    
+    @objc func editDiaryNotification(_ notification: Notification){
+        guard let diary = notification.object as? Diary else { return }
+        guard let row = notification.userInfo?["indexPath.row"] as? Int else { return }
+        self.diary = diary
+        self.configureView()
+    }
+    
+    @IBAction func tapEditButton(_ sender: UIButton) {
+        guard let viewController = self.storyboard?.instantiateViewController(withIdentifier: "WriteDiaryViewController") as? WriteDiaryViewController else { return }
+        guard let indexPath = self.indexPath else { return }
+        guard let diary = self.diary else { return }
+        viewController.diaryEditorMode = .edit(indexPath, diary)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(editDiaryNotification(_:)),
+            name: NSNotification.Name("editDiary"),
+            object: nil)
+        self.navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+    
+    @IBAction func tapDeleteButton(_ sender: UIButton) {
+        guard let indexPath = self.indexPath else { return }
+        self.delegate?.didSelectDelete(indexPath: indexPath)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    @objc func tapStarButton() {
+        guard let isStar = self.diary?.isStar else { return }
+        guard let indexPath = self.indexPath else { return }
+        
+        if isStar {
+            self.starButton?.image = UIImage(systemName: "star")
+        } else {
+            self.starButton?.image = UIImage(systemName: "star.fill")
+        }
+        self.diary?.isStar = !isStar
+        self.delegate?.didSelectStar(indexPath: indexPath, isStar: self.diary?.isStar ?? false)
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
